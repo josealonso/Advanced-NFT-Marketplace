34ae4bc70ecd12b0d025dd3395f44247f9f0d334
josealonso <jose.alonso.programmer@gmail.com>
2022-06-20 03:13:49 +0200

    Added functions prototypes and modified the buyItem function

--- backend/contracts/NFTMarketplace.sol
+++ backend/contracts/NFTMarketplace.sol
@@ -30,12 +30,13 @@ contract NFTMarketplace is ERC721URIStorage {
     }
     mapping(uint256 => MarketItem) private idToMarketItem;
 
-    event MarketItemCreated(
+    // @dev It means written to the blochchain
+    event MarketItemBought(
         uint256 indexed tokenId,
         address seller,
         address owner,
         uint256 price,
-        bool sold
+        bool sold // ??
     );
 
     constructor(
@@ -43,7 +44,7 @@ contract NFTMarketplace is ERC721URIStorage {
         string memory symbol,
         string memory baseURI
     ) ERC721(name, symbol) {
-        baseTokenURI = baseURI;
+        // baseTokenURI = baseURI;
         owner = msg.sender;
     }
 
@@ -65,14 +66,85 @@ contract NFTMarketplace is ERC721URIStorage {
     }
 
     // @dev Mint the NFT token and transfer it to the buyer
-    function buyItem(MarketItem memory _item) external {
+    function buyItem(string memory tokenURI, MarketItem memory _item)
+        external
+        returns (uint256)
+    {
         // make sure the buyer has money enough
-        if (testTokenContract.balanceOf(msg.sender) < _item.price) {
+        uint256 itemPrice = _item.price;
+        if (testTokenContract.balanceOf(msg.sender) < itemPrice) {
             revert("Not enough money");
         }
+        // TODO approve and transfer the ERC-20 token
+        testTokenContract.approve(msg.sender, itemPrice);
+        // testTokenContract.safeTransferFrom(msg.sender, address(this), itemPrice);
+        testTokenContract.transfer(address(this), itemPrice);
+
         // the NFT is minted
-         _tokenIds.increment();
+        _tokenIds.increment();
         uint256 newTokenId = _tokenIds.current();
-        _safeMint(msg.sender, newTokenId); 
+        // _safeMint(msg.sender, newTokenId);
+        _safeMint(address(this), newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        transferMarketItem(newTokenId, itemPrice);
+        return newTokenId;
+    }
+
+    function transferMarketItem(uint256 tokenId, uint256 price) private {
+        // require(price > 0, "Price must be at least 1 wei");
+        // require(
+        //     msg.value == listingPrice,
+        //     "Price must be equal to listing price"
+        // );
+        MarketItem memory marketItem; // = new MarketItem();  // "new" is only for contracts
+        marketItem.tokenId = tokenId;
+        marketItem.owner = msg.sender;
+        marketItem.price = price;
+        // @dev the seller is the own marketplace only for new products
+        marketItem.seller = address(this);
+        marketItem.sold = true;
+        idToMarketItem[tokenId] = marketItem;
+        // idToMarketItem[tokenId] = MarketItem;
+        //     tokenId,
+        //     payable(msg.sender),
+        //     payable(address(this)),
+        //     price,
+        //     true // "sold" field
+        // );
+
+        // _transfer(msg.sender, address(this), tokenId);
+        _transfer(address(this), msg.sender, tokenId);
+        emit MarketItemBought(tokenId, address(this), msg.sender, price, true);
+    }
+
+    function createMarketSale(uint256 tokenId) public payable {
+        // Is it needed ?
+    }
+
+    /* Returns all unsold market items */
+    function fetchMarketItems() public view returns (MarketItem[] memory) {}
+
+    /* Returns only items that a user has purchased */
+    function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        uint256 totalItemCount = _tokenIds.current();
+        uint256 itemCount = 0;
+        uint256 currentIndex = 0;
+
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                itemCount += 1;
+            }
+        }
+
+        MarketItem[] memory items = new MarketItem[](itemCount);
+        for (uint256 i = 0; i < totalItemCount; i++) {
+            if (idToMarketItem[i + 1].owner == msg.sender) {
+                uint256 currentId = i + 1;
+                MarketItem storage currentItem = idToMarketItem[currentId];
+                items[currentIndex] = currentItem;
+                currentIndex += 1;
+            }
+        }
+        return items;
     }
 }
