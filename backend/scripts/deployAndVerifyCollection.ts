9f76e850e3918d3770b9e5162ff97d592335ece0
josealonso <jose.alonso.programmer@gmail.com>
2022-06-29 12:48:41 +0200

    Trying to automate the verification process, but it's not working

---
+++ backend/scripts/deployAndVerifyCollection.ts
@@ -0,0 +1,52 @@
+import hre, { ethers } from "hardhat";
+import { writeFileSync } from "fs";
+import { hrtime } from "process";
+import { setTimeout } from "timers";
+
+async function main() {
+  // URL from where we can extract the metadata for the NFTs
+  const metadataURL = "ipfs://QmZc7jp32oZNbaSGBLbMib4GU5YnGesvudHcE3o7EVqdjs/";
+  const MY_ADDRESS = "0x5f793abbd751f1ac5b0f95f3c3d117e5fd218c41";    //"0xFE2de2924c17C5A5E351E5fD13E2657836716BdD";
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so collectionContract here is a factory for instances of our NFTCollection contract.
+  */
+  const collectionContract = await ethers.getContractFactory("NFTCollection");
+
+  // deploy the contract
+  const deployedCollectionContract = await collectionContract.deploy("Wood Tables", "WTB", 40, "Wood Tables", MY_ADDRESS);
+  await deployedCollectionContract.deployed();
+  // print the address of the deployed contract
+  console.log("Collection Contract Address:", deployedCollectionContract.address);
+
+  // Verify the contract
+  const waitFor = (delay: number) => new Promise(resolve => setTimeout(() => {
+    hre.run("verify:verify", {
+      address: deployedCollectionContract.address,
+      constructorArguments: ["Wood Tables", "WTB", 40, "Wood Tables", MY_ADDRESS],
+    })
+  }, delay));
+  await waitFor(60000);
+
+  // To be changed
+  // writeFileSync('../frontend/config.ts', `export const marketplaceAddress = "${deployedCollectionContract.address}"
+  //   `);
+
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+/*
+* DEPLOY
+* `npx hardhat run scripts/deployCollection.ts --network mumbai`
+*
+* VERIFY
+*  `npx hardhat verify 0x5e3dF86A4C35885E1C6A1492970d6708CBAECaaf "Wood Tables" "WTB" 40 "Wood Tables" 0xFE2de2924c17C5A5E351E5fD13E2657836716BdD --network mumbai`
+*  (without commas)
+*/
