cc735ef9638c3f937bde8fb4a37f177131d7a604
josealonso <jose.alonso.programmer@gmail.com>
2022-06-29 03:07:16 +0200

    Deployment scripts

--- backend/scripts/deployMarketplace.ts
+++ backend/scripts/deployMarketplace.ts
@@ -2,21 +2,21 @@ import { ethers } from "hardhat";
 import { writeFileSync } from "fs";
 
 async function main() {
-  // URL from where we can extract the metadata for the NFTs
-  const metadataURL = "ipfs://QmZc7jp32oZNbaSGBLbMib4GU5YnGesvudHcE3o7EVqdjs/";
+  const FEE_PERCENT = 1;
+
   /*
   A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so punksContract here is a factory for instances of our Punks contract.
+  so marketplaceContract here is a factory for instances of our FinalMarketplace contract.
   */
-  const marketplaceContract = await ethers.getContractFactory("NFTMarketplace");
+  const marketplaceContract = await ethers.getContractFactory("FinalMarketplace");
 
   // deploy the contract
-  // const deployedMarketplaceContract = await marketplaceContract.deploy(metadataURL);
-  const deployedMarketplaceContract = await marketplaceContract.deploy();
+  const deployedMarketplaceContract = await marketplaceContract.deploy(FEE_PERCENT);
   await deployedMarketplaceContract.deployed();
   // print the address of the deployed contract
   console.log("Marketplace Contract Address:", deployedMarketplaceContract.address);
 
+  // To be changed
   writeFileSync('../frontend/config.ts', `export const marketplaceAddress = "${deployedMarketplaceContract.address}"
   `);
 }
@@ -28,3 +28,4 @@ main()
     console.error(error);
     process.exit(1);
   });
+
